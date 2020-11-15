88d155145dd8f6c45b63daf29f93d8675d1ac422
Cem Saltık <53943895+cemsltk@users.noreply.github.com>
2020-11-15 12:47:58 +0300

    Update WinningTeam.js

--- WinningTeam.js
+++ WinningTeam.js
@@ -25,9 +25,23 @@ $(document).ready(function() {
                 jQuery('.output label').text(winningTeam);
                 jQuery('.output').show();
             }
+        }          
+    });   
+    function validate(evt) {
+        var theEvent = evt || window.event;
+      
+        // Handle paste
+        if (theEvent.type === 'paste') {
+            key = event.clipboardData.getData('text/plain');
+        } else {
+        // Handle key press
+            var key = theEvent.keyCode || theEvent.which;
+            key = String.fromCharCode(key);
         }
-        
-        
-    });
-
+        var regex = /[0-9]|\./;
+        if( !regex.test(key) ) {
+          theEvent.returnValue = false;
+          if(theEvent.preventDefault) theEvent.preventDefault();
+        }
+      }
 });
